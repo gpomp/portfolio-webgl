02db8d787f0dcab2d290af7533395c38934792d3
Gauthier Pompougnac <gauthier.pompougnac@gmail.com>
2018-05-17 11:56:11 -0700

    video instead of animation at the top

--- gulpfile.js
+++ gulpfile.js
@@ -1,178 +1,214 @@
-var gulp = require('gulp'),
-    fs = require('fs'),
-	gulpif = require('gulp-if'),
-	gutil = require('gulp-util'),
-	watch = require('gulp-watch'),
-	plumber = require('gulp-plumber'),
-	less = require('gulp-less'),
-	minifycss = require('gulp-minify-css'),
-	modernizr = require('gulp-modernizr'),
-	concat = require('gulp-concat-sourcemap'),
-	sourcemaps = require('gulp-sourcemaps'),
-	uglify = require('gulp-uglify'),
-	concat = require('gulp-concat'),
-	iconfont = require('gulp-iconfont'),
-	iconfontCss = require('gulp-iconfont-css'),
-	debug = require('gulp-debug'),
-	runSequence = require('run-sequence').use(gulp),
-    source = require('vinyl-source-stream'),
-    buffer = require('vinyl-buffer'),
-    browserify = require('browserify'),
-    babelify = require('babelify'),
-    glslify = require('glslify'),
-    watchify = require('watchify');
+var gulp = require("gulp"),
+  fs = require("fs"),
+  gulpif = require("gulp-if"),
+  gutil = require("gulp-util"),
+  watch = require("gulp-watch"),
+  plumber = require("gulp-plumber"),
+  less = require("gulp-less"),
+  minifycss = require("gulp-minify-css"),
+  modernizr = require("gulp-modernizr"),
+  concat = require("gulp-concat-sourcemap"),
+  sourcemaps = require("gulp-sourcemaps"),
+  uglify = require("gulp-uglify"),
+  concat = require("gulp-concat"),
+  iconfont = require("gulp-iconfont"),
+  iconfontCss = require("gulp-iconfont-css"),
+  debug = require("gulp-debug"),
+  runSequence = require("run-sequence").use(gulp),
+  source = require("vinyl-source-stream"),
+  buffer = require("vinyl-buffer"),
+  browserify = require("browserify"),
+  babelify = require("babelify"),
+  glslify = require("glslify"),
+  watchify = require("watchify");
 
 var vendorExternal = [
-    { require: 'detect-dom-ready' },
-    { require: 'dom-select' },
-    { require: 'dom-event' },
-    { require: 'dom-classes' },
-    { require: 'raf' },
-    { require: 'glslify' },
-    { require: 'gsap/src/uncompressed/TweenLite' },
-    { require: 'gsap/src/uncompressed/plugins/ScrollToPlugin' },
-    { require: 'gsap/src/uncompressed/easing/EasePack' }
+  { require: "detect-dom-ready" },
+  { require: "dom-select" },
+  { require: "dom-event" },
+  { require: "dom-classes" },
+  { require: "raf" },
+  { require: "glslify" },
+  { require: "gsap/src/uncompressed/TweenLite" },
+  { require: "gsap/src/uncompressed/plugins/ScrollToPlugin" },
+  { require: "gsap/src/uncompressed/easing/EasePack" }
 ];
 
 var config = {
-	'env': 'prod',
-	'theme' : 'web/app/themes/portfolio/'
+  env: "prod",
+  theme: "web/app/themes/portfolio/"
 };
 
 config.base = config.app;
 
-gulp.task('less', function () {
-	return gulp.src('less/main.less')
-		.pipe(plumber())
-		.pipe(less().on('error', gutil.log))
-		.pipe(gulpif(config.env === 'prod', minifycss()))
-		.pipe(gulp.dest(config.theme + 'css'));
+gulp.task("less", function() {
+  return gulp
+    .src("less/main.less")
+    .pipe(plumber())
+    .pipe(less().on("error", gutil.log))
+    .pipe(gulpif(config.env === "prod", minifycss()))
+    .pipe(gulp.dest(config.theme + "css"));
 });
 
-gulp.task('modernizr', function() {
-    return gulp.src([config.theme + 'js/vendors.js', config.theme + 'js/app.js', config.theme + 'css/main.css'])
-    .pipe(concat('temp'))
-    .pipe(modernizr({
-        'options' : [
-            'setClasses',
-            'addTest',
-            'html5printshiv',
-            'testProp',
-            'fnBind',
-            'mq',
-            'prefixed'
+gulp.task("modernizr", function() {
+  return gulp
+    .src([
+      config.theme + "js/vendors.js",
+      config.theme + "js/app.js",
+      config.theme + "css/main.css"
+    ])
+    .pipe(concat("temp"))
+    .pipe(
+      modernizr({
+        options: [
+          "setClasses",
+          "addTest",
+          "html5printshiv",
+          "testProp",
+          "fnBind",
+          "mq",
+          "prefixed"
         ],
-        'tests': [
-            'csstransforms3d',
-            'preserve3d',
-            'touchevents'
-        ],
-        'crawl' : true
-    }))
-    .pipe(gulpif(config.env === 'prod', uglify({mangle: true, compress : {drop_console:true}})))
-    .pipe(gulp.dest(config.theme + 'js/'));
-})
+        tests: ["csstransforms3d", "preserve3d", "touchevents"],
+        crawl: true
+      })
+    )
+    .pipe(
+      gulpif(
+        config.env === "prod",
+        uglify({ mangle: true, compress: { drop_console: true } })
+      )
+    )
+    .pipe(gulp.dest(config.theme + "js/"));
+});
 
 // Glyphs
-var fontName = 'glyph';
-gulp.task('font', function(){
-    return gulp.src('assets/glyph/*.svg')
-        .pipe(iconfontCss({
-            fontName: fontName,
-            path: 'node_modules/gulp-iconfont-css/templates/_icons.less',
-            targetPath: '../../../../../less/generated/glyph.less',
-            fontPath: '../fonts/'
-        }))
-        .pipe(iconfont({
-            fontName: fontName
-        }))
-        .pipe(gulp.dest(config.theme+'fonts/'));
+var fontName = "glyph";
+gulp.task("font", function() {
+  return gulp
+    .src("assets/glyph/*.svg")
+    .pipe(
+      iconfontCss({
+        fontName: fontName,
+        path: "node_modules/gulp-iconfont-css/templates/_icons.less",
+        targetPath: "../../../../../less/generated/glyph.less",
+        fontPath: "../fonts/"
+      })
+    )
+    .pipe(
+      iconfont({
+        fontName: fontName
+      })
+    )
+    .pipe(gulp.dest(config.theme + "fonts/"));
 });
 
-
-gulp.task('build-vendor', function () {
+gulp.task("build-vendor", function() {
   var bVendor = browserify({
-        debug: config.env === 'dev'
+    debug: config.env === "dev"
+  });
+
+  vendorExternal.forEach(function(external) {
+    bVendor.require(external.require);
+  });
+
+  return bVendor
+    .bundle()
+    .pipe(source("vendor.js"))
+    .pipe(buffer())
+    .pipe(
+      gulpif(
+        config.env === "prod",
+        uglify({ mangle: true, compress: { drop_console: true } })
+      )
+    )
+    .on("error", gutil.log)
+    .pipe(gulp.dest(config.theme + "js"));
+});
+
+gulp.task("build-app", function() {
+  var browserifyOptions = {
+    entries: ["./js/index.js"],
+    debug: config.env === "dev",
+    cache: {},
+    packageCache: {}
+  };
+
+  var bApp =
+    config.env === "dev"
+      ? watchify(browserify(browserifyOptions))
+      : browserify(browserifyOptions);
+
+  bApp.transform("babelify", { presets: ["es2015"] }).transform("glslify");
+
+  vendorExternal.forEach(function(external) {
+    bApp.external(external.require);
+  });
+
+  // var watcher = watchify(bApp);
+
+  if (config.env === "dev") {
+    gutil.log("watch ->");
+
+    bApp.on("time", function(time) {
+      gutil.log("-> watchify on time", time);
+    });
+    bApp.on("bytes", function(bytes) {
+      gutil.log("-> watchify on bytes", bytes);
     });
 
-    vendorExternal.forEach(function(external) {
-        bVendor.require(external.require);
+    bApp.on("update", function() {
+      gutil.log("-> watchify on update");
+      rebundle();
     });
-
-    return bVendor.bundle()
-        .pipe(source('vendor.js'))
-        .pipe(buffer())
-        .pipe(gulpif(config.env === 'prod', uglify({mangle: true, compress : {drop_console:true}})))
-        .on('error', gutil.log)
-        .pipe(gulp.dest(config.theme + 'js'));
-});
-
-gulp.task('build-app', function () {
-    var browserifyOptions = {
-        entries: ['./js/index.js'],
-        debug: config.env === 'dev',
-        cache: {},
-        packageCache: {}
-    };
- 
-    var bApp = config.env === 'dev' ? watchify(browserify(browserifyOptions)) : browserify(browserifyOptions);
-
-    bApp.transform('babelify', { presets: ['es2015'] })
-        .transform('glslify');
-
-    vendorExternal.forEach(function(external) {
-        bApp.external(external.require);
+    bApp.on("log", function(message) {
+      gutil.log("-> watchify", message);
     });
+  }
 
-    // var watcher = watchify(bApp);
+  function rebundle() {
+    gutil.log("-> bundle...");
+    return bApp
+      .bundle()
+      .on("error", gutil.log)
+      .pipe(source("app.js"))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(
+        gulpif(
+          config.env === "prod",
+          uglify({ mangle: true, compress: { drop_console: true } })
+        )
+      )
+      .pipe(sourcemaps.write("./"))
+      .pipe(gulp.dest(config.theme + "js"));
+  }
 
-    if(config.env === 'dev') {
-        gutil.log('watch ->');
-
-        bApp.on('time', function (time) { gutil.log('-> watchify on time', time); });
-        bApp.on('bytes', function (bytes) { gutil.log('-> watchify on bytes', bytes); });
-        
-        bApp.on('update', function () {
-            gutil.log('-> watchify on update');
-            rebundle();
-        });
-        bApp.on('log', function (message) {
-            gutil.log('-> watchify', message);
-        });
-    }
-
-    function rebundle () {
-        gutil.log('-> bundle...');
-        return bApp.bundle()        
-        .on('error', gutil.log)
-        .pipe(source('app.js'))
-        .pipe(buffer())
-        .pipe(sourcemaps.init({loadMaps: true}))
-        .pipe(gulpif(config.env === 'prod', uglify({mangle: true, compress : {drop_console:true}})))
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest(config.theme + 'js'));
-    }
-
-    return rebundle();
-
-});
- 
-gulp.task('watch', function () {
-	gulp.watch('less/*.less', ['less']);
-    // gulp.watch(['./js/**/*.js', './js/**/*.glsl'], ['build-app']);
+  return rebundle();
 });
 
-gulp.task('devconfig', function () {
-    gutil.log('set env as dev...');
-    config.env = 'dev';
+gulp.task("watch", function() {
+  gulp.watch("less/*.less", ["less"]);
+  // gulp.watch(['./js/**/*.js', './js/**/*.glsl'], ['build-app']);
 });
 
-gulp.task('dev', function(callback) {
-    runSequence('devconfig',['build-vendor', 'build-app', 'less', 'watch'] );
+gulp.task("devconfig", function() {
+  gutil.log("set env as dev...");
+  config.env = "dev";
 });
 
-gulp.task('prod', function(callback) {
-    runSequence(['build-vendor', 'build-app', 'less'], 'modernizr' );
+gulp.task("dev", function(callback) {
+  runSequence("devconfig", [
+    "font",
+    "build-vendor",
+    "build-app",
+    "less",
+    "watch"
+  ]);
 });
 
-gulp.task('default',['prod']);
+gulp.task("prod", function(callback) {
+  runSequence(["font", "build-vendor", "build-app", "less"], "modernizr");
+});
+
+gulp.task("default", ["prod"]);
--- js/GlobalPage.js
+++ js/GlobalPage.js
@@ -1,123 +1,128 @@
-'use strict';
+"use strict";
 
-var select = require('dom-select');
-var classes = require('dom-classes');
-var Tween = require('gsap/src/uncompressed/TweenLite');
-var ScrollToPlugin = require('gsap/src/uncompressed/plugins/ScrollToPlugin');
-var EasePack = require('gsap/src/uncompressed/easing/EasePack');
-var on = require('dom-event');
+var select = require("dom-select");
+var classes = require("dom-classes");
+var Tween = require("gsap/src/uncompressed/TweenLite");
+var ScrollToPlugin = require("gsap/src/uncompressed/plugins/ScrollToPlugin");
+var EasePack = require("gsap/src/uncompressed/easing/EasePack");
+var on = require("dom-event");
 
 // var Intro = require('./intro/');
- 
+
 class GlobalPage {
-    constructor() {
-        console.log('global page');
-        this.init();
+  constructor() {
+    console.log("global page");
+    this.init();
+  }
+
+  resize(event) {
+    select("#home-intro").style.marginTop = window.innerHeight + "px";
+  }
+
+  init() {
+    _gaq.push(["_trackPageview", "/nowebgl"]);
+    on(window, "resize", this.resize.bind(this));
+    this.resize();
+
+    // var intro = new  Intro();
+
+    classes(select("html")).add("no-threejs");
+    select(".overlay-nowebgl").style.display = "none";
+
+    /*Buttons*/
+    classes(select("#projects-buttons")).add("show");
+    var buttons = select.all("#projects-buttons a");
+
+    for (let i = 0; i < buttons.length; ++i) {
+      let button = buttons[i];
+      classes(button).add("show");
+      button.setAttribute("href", "#" + button.getAttribute("href"));
+      var self = this;
+      button.addEventListener("click", function(event) {
+        self.clickProject(event, this);
+      });
     }
 
-    resize (event) {
-        select("#home-intro").style.marginTop = (window.innerHeight - select("#home-intro .intro-p").offsetHeight) + "px";
+    /*Projects*/
+    classes(select("#projects")).add("show");
+    var projects = select.all("#projects .project");
+
+    for (let i = 0; i < projects.length; ++i) {
+      let project = projects[i];
+      classes(project).add("show");
+      classes(select(".text", project)).add("show");
+
+      var images = select.all("img", project);
+      for (let j = 0; j < images.length; ++j) {
+        let img = images[j];
+        img.setAttribute("src", img.getAttribute("data-src"));
+      }
     }
 
-    init() {
-        _gaq.push(['_trackPageview', '/nowebgl']);
-        on(window, 'resize', this.resize.bind(this));
-        this.resize();
+    window.addEventListener("scroll", this.scroll.bind(this));
 
-        // var intro = new  Intro(); 
-
-        classes(select('html')).add('no-threejs');
-        select('.overlay-nowebgl').style.display = 'none';
-             
-        /*Buttons*/
-        classes(select('#projects-buttons')).add("show");
-        var buttons = select.all('#projects-buttons a');
-
-        for (let i = 0; i < buttons.length; ++i) {
-            let button = buttons[i];
-            classes(button).add('show');
-            button.setAttribute("href", "#" + button.getAttribute("href"));
-            var self = this;
-            button.addEventListener("click", function(event) {
-                self.clickProject(event, this);
-            });
-        }
-
-        /*Projects*/
-        classes(select('#projects')).add('show');
-        var projects = select.all('#projects .project');
-
-        for (let i = 0; i < projects.length; ++i) {
-            let project = projects[i];
-            classes(project).add('show');
-            classes(select('.text', project)).add('show');
-
-            var images = select.all('img', project);
-            for (let j = 0; j < images.length; ++j) {
-                let img = images[j];
-                img.setAttribute("src", img.getAttribute("data-src"));
-            }
-        }
-
-        window.addEventListener('scroll', this.scroll.bind(this));
-
-        window.setTimeout(function() { this.checkScroll(); }.bind(this), 1000);
-    }
-
-    scroll(event) {
+    window.setTimeout(
+      function() {
         this.checkScroll();
-    }
+      }.bind(this),
+      1000
+    );
+  }
 
-    checkScrollAnim() {
-        this.checkScroll();
-    }
+  scroll(event) {
+    this.checkScroll();
+  }
 
-    checkScroll() {
-        let h = window.innerHeight;
-        let projects = select.all('#projects .project');
-        let cProject = null;  
-        let biggest = -1;
-        let big = 0; 
+  checkScrollAnim() {
+    this.checkScroll();
+  }
 
-        for (let i = 0; i < projects.length; ++i) {
-            let node = projects[i];
-            let br = node.getBoundingClientRect();
-            let t = br.top;
-            let b = br.bottom;
-            let perc = Math.max(0, t);
-            if(t - h < 0 && b - h > 0) {
-                if(!classes.has(node, 'anim')) {
-                    classes.add(node, 'anim');
-                }
+  checkScroll() {
+    let h = window.innerHeight;
+    let projects = select.all("#projects .project");
+    let cProject = null;
+    let biggest = -1;
+    let big = 0;
 
-                biggest = i;
-                
-            }
- 
+    for (let i = 0; i < projects.length; ++i) {
+      let node = projects[i];
+      let br = node.getBoundingClientRect();
+      let t = br.top;
+      let b = br.bottom;
+      let perc = Math.max(0, t);
+      if (t - h < 0 && b - h > 0) {
+        if (!classes.has(node, "anim")) {
+          classes.add(node, "anim");
         }
-        if(biggest !== -1) {
-            let btn = select.all('#projects-buttons a')[biggest];
-            if(!classes.has(btn, 'active')) {
-                let btnList = select.all('#projects-buttons a');
-                for (let i = 0; i < btnList.length; ++i) {
-                    classes.remove(btnList[i], 'active');
-                }
-                classes.add(btn, 'active');
-            }
 
+        biggest = i;
+      }
+    }
+    if (biggest !== -1) {
+      let btn = select.all("#projects-buttons a")[biggest];
+      if (!classes.has(btn, "active")) {
+        let btnList = select.all("#projects-buttons a");
+        for (let i = 0; i < btnList.length; ++i) {
+          classes.remove(btnList[i], "active");
         }
+        classes.add(btn, "active");
+      }
     }
+  }
 
-    clickProject(event, link) {
-        event.preventDefault();
+  clickProject(event, link) {
+    event.preventDefault();
 
-        let name = link.getAttribute('name');
+    let name = link.getAttribute("name");
 
-        let project = select('#projects .project.' + name);
-        let ctnTop = select('#projects').offsetTop;
-        Tween.to(window, 1, {scrollTo:{ y: ctnTop + project.offsetTop }, ease: Expo.easeInOut, onComplete:this.checkScrollAnim.bind(this)});
-    }
-} 
-
+    let project = select("#projects .project." + name);
+    let ctnTop = select("#projects").offsetTop;
+    Tween.to(window, 1, {
+      scrollTo: { y: ctnTop + project.offsetTop },
+      ease: Expo.easeInOut,
+      onComplete: this.checkScrollAnim.bind(this)
+    });
+  }
+}
 
 module.exports = GlobalPage;
